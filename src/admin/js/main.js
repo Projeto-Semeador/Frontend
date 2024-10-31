32f5a2fe11f9873456195c4f3c00675f27ea02cb
Matelz <49626198+Matelz@users.noreply.github.com>
2024-10-31 16:35:01 -0300

    refactor: abstracted functions to factories

--- src/admin/js/main.js
+++ src/admin/js/main.js
@@ -1,35 +1,74 @@
 const serverURL = 'http://localhost:3000';
 
-async function login() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const remember = document.getElementById('remember').checked;
+const defaultOptions = {
+  withCredentials: true,
+};
 
-  const data = {
-    username: email,
-    password,
-    remember,
-  };
-
-  await axios
-    .post(`${serverURL}/login`, data, {
-      withCredentials: remember,
-    })
-    .then((res) => {
-      window.location.href = '/src/admin/dashboard';
-    })
-    .catch((err) => {
-      let alert = document.querySelector('.alert');
-      alert.className = `alert alert-danger show`;
-      alert.classList.remove('d-none');
-
-      setTimeout(() => {
-        alert.classList.remove('show');
-        alert.classList.add('d-none');
-      }, 5000);
-    });
+// Factory function for the event card
+function eventTemplateFactory(event) {
+  return `
+    <div class="card">
+      <div class="card-header">
+          Evento #${event.id}
+      </div>
+      <div class="card-body">
+      <div class="d-flex justify-content-between align-items-center">
+          <div>
+          <h5 class="card-title">${event.name}</h5>
+          <p class="card-text">
+            ${event.description}
+          </p>
+          </div>
+          <div class="d-flex gap-2" style="height: min-content;">
+            <button href="#" class="btn btn-primary text-white"><i class="bi bi-pencil"></i></button>
+            <button data-bs-toggle="modal" class="btn btn-danger" data-id="${event.id}" data-bs-target="#deleteEventModal" ><i class="bi bi-trash"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 }
 
+// Factory function for the user card
+function userTemplateFactory(user, id) {
+  return `
+    <div class="card">
+      <div class="card-header">
+          Usuário #${id}
+      </div>
+      <div class="card-body">
+      <div class="d-flex gap-2 align-items-center justify-content-between">
+          <h5>${user.username}</h5>
+          <div>
+            <button href="#" class="btn btn-primary text-white"><i class="bi bi-pencil"></i></button>
+            <button data-bs-toggle="modal" class="btn btn-danger" data-username="${user.username}" data-bs-target="#deleteUserModal"><i class="bi bi-trash"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+// Factory function for the analytics card
+function analyticsTemplateFactory(analytics) {
+  return `
+    <div class="col-md-6 mb-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${analytics.name}</h5>
+            <p class="card-text">${analytics.description}</p>
+              <div
+              class="d-flex justify-content-between align-items-center"
+              >
+              ${analytics.type == 'chart' ? `<canvas id="${analytics.metric}Chart"></canvas>` : `<h1 id="${analytics.metric}">${analytics.value}</h1><i class="bi bi-bar-chart"></i>`}
+            </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+// Displays an alert on the screen
 function displayAlert(type, message) {
   var icon;
 
@@ -58,6 +97,7 @@ function displayAlert(type, message) {
   }, 5000);
 }
 
+// Populates the events list with the events fetched from the server
 function populateEvents(events) {
   const eventList = document.querySelector('#eventList');
   eventList.innerHTML = '';
@@ -71,30 +111,11 @@ function populateEvents(events) {
   }
 
   events.forEach((e) => {
-    eventList.innerHTML += `
-			<div class="card">
-				<div class="card-header">
-						Evento #${e.id}
-				</div>
-				<div class="card-body">
-				<div class="d-flex justify-content-between align-items-center">
-						<div>
-						<h5 class="card-title">${e.name}</h5>
-						<p class="card-text">
-					${e.description}
-						</p>
-						</div>
-						<div class="d-flex gap-2" style="height: min-content;">
-							<button href="#" class="btn btn-primary text-white"><i class="bi bi-pencil"></i></button>
-							<button data-bs-toggle="modal" class="btn btn-danger" data-id="${e.id}" data-bs-target="#deleteEventModal" ><i class="bi bi-trash"></i></button>
-						</div>
-					</div>
-				</div>
-			</div>
-		`;
+    eventList.innerHTML += eventTemplateFactory(e);
   });
 }
 
+// Populates the users list with the users fetched from the server
 function populateUsers(users) {
   const userList = document.querySelector('#userList');
   userList.innerHTML = '';
@@ -108,157 +129,22 @@ function populateUsers(users) {
   }
 
   users.forEach((u) => {
-    userList.innerHTML += `
-			<div class="card">
-				<div class="card-header">
-						Usuário #${users.indexOf(u) + 1}
-				</div>
-				<div class="card-body">
-				<div class="d-flex gap-2 align-items-center justify-content-between">
-						<h5>${u.username}</h5>
-						<div>
-							<button href="#" class="btn btn-primary text-white"><i class="bi bi-pencil"></i></button>
-							<button data-bs-toggle="modal" class="btn btn-danger" data-username="${u.username}" data-bs-target="#deleteUserModal"><i class="bi bi-trash"></i></button>
-						</div>
-					</div>
-				</div>
-			</div>
-		`;
+    userList.innerHTML += userTemplateFactory(u, users.indexOf(u) + 1);
   });
 }
 
-async function fetchEvents() {
-  await axios.get(`${serverURL}/events`).then((res) => {
-    populateEvents(res.data);
-  });
-}
-
-async function fetchUsers() {
-  await axios
-    .get(`${serverURL}/users`, {
-      withCredentials: true,
-    })
-    .then((res) => {
-      populateUsers(res.data);
-    });
-}
-
-async function createNewEvent() {
-  let eventName = document.querySelector('#eventName').value;
-  let eventDescription = document.querySelector('#eventDescription').value;
-  let eventImage = document.querySelector('#eventImage').files[0];
-  let modal = document.querySelector('#addEventModal');
-
-  const event = {
-    name: eventName,
-    description: eventDescription,
-    image: eventImage,
-  };
-
-  console.log(event);
-
-  await axios
-    .post(`${serverURL}/events`, event, {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-    .then((res) => {
-      displayAlert('success', 'Evento criado com sucesso');
-    })
-    .catch((err) => {
-      displayAlert('danger', 'Não foi possível criar o evento');
-    });
-
-  await fetchEvents();
-
-  $('#addEventModal').modal('toggle');
-}
-
-async function deleteEvent(id) {
-  await axios
-    .delete(`${serverURL}/events/${id}`, {
-      withCredentials: true,
-    })
-    .then((res) => {
-      displayAlert('success', 'Evento deletado com sucesso');
-    })
-    .catch((err) => {
-      displayAlert('danger', 'Não foi possível deletar o evento');
-    });
-
-  await fetchEvents();
-
-  $('#deleteEventModal').modal('toggle');
-}
-
-$('#deleteEventModal').on('show.bs.modal', function (event) {
-  let button = $(event.relatedTarget);
-  let id = button.data('id');
-
-  let modal = $(this);
-  modal.find('#deleteConfirmation').attr('onclick', `deleteEvent(${id})`);
-});
-
-async function createUser() {
-  let username = document.querySelector('#username').value;
-  let password = document.querySelector('#password').value;
-  let passwordConfirmation = document.querySelector(
-    '#passwordConfirmation'
-  ).value;
-
-  const user = {
-    user: {
-      username,
-      password,
-    },
-  };
-
-  if (password !== passwordConfirmation) {
-    $('#passwordConfirmation').addClass('is-invalid');
-    return;
-  }
-
-  await axios
-    .post(`${serverURL}/register`, user, {
-      withCredentials: true,
-    })
-    .then((res) => {
-      displayAlert('success', 'Usuário criado com sucesso');
-      fetchUsers();
-      $('#addUserModal').modal('toggle');
-    })
-    .catch((err) => {
-      displayAlert('danger', 'Não foi possível criar o usuário');
-    });
-}
-
+// Fetches the analytics from the server and populates the analytics table
 function populateAnalytics(analytics) {
   const analyticsTable = document.querySelector('#analyticsTable');
 
   analyticsTable.innerHTML = '';
 
   analytics.forEach((a) => {
-    analyticsTable.innerHTML += `
-			<div class="col-md-6 mb-4">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">${a.name}</h5>
-							<p class="card-text">${a.description}</p>
-								<div
-								class="d-flex justify-content-between align-items-center"
-								>
-								${a.type == 'chart' ? `<canvas id="${a.metric}Chart"></canvas>` : `<h1 id="${a.metric}">${a.value}</h1><i class="bi bi-bar-chart"></i>`}
-							</div>
-					</div>
-				</div>
-			</div>
-		`;
-
+    analyticsTable.innerHTML += analyticsTemplateFactory(a);
+    
     if (a.type == 'chart') {
       const ctx = document.getElementById(`${a.metric}Chart`);
-
+  
       new Chart(ctx, {
         type: 'bar',
         data: {
@@ -289,23 +175,146 @@ function populateAnalytics(analytics) {
   });
 }
 
-async function fetchAnalytics() {
+// Logs the user in
+async function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const remember = document.getElementById('remember').checked;
+
+  const data = {
+    username: email,
+    password,
+    remember,
+  };
+
   await axios
-    .get(`${serverURL}/analytics`, {
+    .post(`${serverURL}/login`, data, {
       withCredentials: true,
     })
+    .then((res) => {
+      jwtToken = getCookie('jwtToken');
+      window.location.href = '/src/admin/dashboard';
+    })
+    .catch((err) => {
+      let alert = document.querySelector('.alert');
+      alert.className = `alert alert-danger show`;
+      alert.classList.remove('d-none');
+
+      setTimeout(() => {
+        alert.classList.remove('show');
+        alert.classList.add('d-none');
+      }, 5000);
+    });
+}
+
+// Registers a new user
+async function createUser() {
+  let username = document.querySelector('#username').value;
+  let password = document.querySelector('#password').value;
+  let passwordConfirmation = document.querySelector(
+    '#passwordConfirmation'
+  ).value;
+
+  const user = {
+    user: {
+      username,
+      password,
+    },
+  };
+
+  if (password !== passwordConfirmation) {
+    $('#passwordConfirmation').addClass('is-invalid');
+    return;
+  }
+
+  await axios
+    .post(`${serverURL}/register`, user, defaultOptions)
+    .then((res) => {
+      displayAlert('success', 'Usuário criado com sucesso');
+      fetchUsers();
+      $('#addUserModal').modal('toggle');
+    })
+    .catch((err) => {
+      displayAlert('danger', 'Não foi possível criar o usuário');
+    });
+}
+
+// Fetches the analytics from the server
+async function fetchAnalytics() {
+  await axios
+    .get(`${serverURL}/analytics`, defaultOptions)
     .then((res) => {
       populateAnalytics(res.data);
     });
 }
 
+// Fetches the events from the server
+async function fetchEvents() {
+  await axios.get(`${serverURL}/events`).then((res) => {
+    populateEvents(res.data);
+  });
+}
+
+// Fetches the users from the server
+async function fetchUsers() {
+  await axios
+    .get(`${serverURL}/users`, defaultOptions)
+    .then((res) => {
+      populateUsers(res.data);
+    });
+}
+
+// Creates a new event
+async function createNewEvent() {
+  let eventName = document.querySelector('#eventName').value;
+  let eventDescription = document.querySelector('#eventDescription').value;
+  let eventImage = document.querySelector('#eventImage').files[0];
+  let modal = document.querySelector('#addEventModal');
+
+  const event = {
+    name: eventName,
+    description: eventDescription,
+    image: eventImage,
+  };
+
+  console.log(event);
+
+  await axios
+    .post(`${serverURL}/events`, event, defaultOptions)
+    .then((res) => {
+      displayAlert('success', 'Evento criado com sucesso');
+    })
+    .catch((err) => {
+      displayAlert('danger', 'Não foi possível criar o evento');
+    });
+
+  await fetchEvents();
+
+  $('#addEventModal').modal('toggle');
+}
+
+// Deletes an event
+async function deleteEvent(id) {
+  await axios
+    .delete(`${serverURL}/events/${id}`, defaultOptions)
+    .then((res) => {
+      displayAlert('success', 'Evento deletado com sucesso');
+    })
+    .catch((err) => {
+      displayAlert('danger', 'Não foi possível deletar o evento');
+    });
+
+  await fetchEvents();
+
+  $('#deleteEventModal').modal('toggle');
+}
+
+// Deletes a user
 async function deleteUser(username) {
   console.log(username);
 
   await axios
-    .delete(`${serverURL}/users/${username}`, {
-      withCredentials: true,
-    })
+    .delete(`${serverURL}/users/${username}`, defaultOptions)
     .then((res) => {
       displayAlert('success', 'Usuário deletado com sucesso');
     })
@@ -317,6 +326,8 @@ async function deleteUser(username) {
   $('#deleteUserModal').modal('toggle');
 }
 
+
+// Sets the event listeners for the modals
 $('#deleteUserModal').on('show.bs.modal', function (event) {
   let button = $(event.relatedTarget);
   let username = button.data('username');
@@ -325,4 +336,12 @@ $('#deleteUserModal').on('show.bs.modal', function (event) {
   modal
     .find('#deleteUserConfirmation')
     .attr('onclick', `deleteUser("${username}")`);
+});
+
+$('#deleteEventModal').on('show.bs.modal', function (event) {
+  let button = $(event.relatedTarget);
+  let id = button.data('id');
+
+  let modal = $(this);
+  modal.find('#deleteConfirmation').attr('onclick', `deleteEvent(${id})`);
 });
