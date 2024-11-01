5094255338d1c9400e36468b1446d1b606813360
Isabella Passarelli <isabellapassarelli025@gmail.com>
2024-11-01 19:40:43 -0300

    deleted the action of manually opening and closing the dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,3 @@
-// On change anchor, change active class
-document.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('.nav-link');
-  const dropdownToggle = document.getElementById('navbarDropdown');
-  const dropdownMenu = document.querySelector('.dropdown-menu');
-
-  links.forEach((link) => {
-    link.addEventListener('click', (e) => {
-      links.forEach((link) => {
-        link.classList.remove('active');
-      });
-      e.target.classList.add('active');
-    });
-  });
-
-  //open and close the dropdown
-  dropdownToggle.addEventListener('clik', (e) => {
-    e.preventDefault();
-    dropdownMenu.classList.toggle('show');
-  });
-  
-  // close the dropdown if you clik outside of it
-  document.addEventListener('click', (e) => {
-    if(!dropdownToggle.contains(e.target) && !dropdownMenu.contains(e.target)){
-      dropdownMenu.classList.remove('show')
-    }
-  });
-});
-
 // Get anchor id from URL
 const re = new RegExp('(#[a-zA-Z0-9]*)');
 const url = window.location.href;
@@ -50,7 +21,7 @@ $('.carousel').carousel({
 // form verification
 document.getElementById("contactForm").addEventListener("submit", function(event) {
   if (!this.checkValidity()) {
-    event.preventDefault(); // Impede o envio se houver campos inválidos
+    event.preventDefault();
     alert("Por favor, preencha todos os campos obrigatórios.");
   }
 });
